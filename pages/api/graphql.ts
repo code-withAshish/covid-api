181292ba0b6ec3a310f9e671f3634fc02e328dfe
code-withAshish <abhushan064@gmail.com>
2022-12-30 17:35:40 +0530

    add graphql

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginLandingPageProductionDefault } from "@apollo/server/plugin/landingPage/default"
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { schema } from "../../graphql/schema"
 import { resolvers } from "../../graphql/resolvers"
@@ -7,6 +8,9 @@ import { resolvers } from "../../graphql/resolvers"
 const server = new ApolloServer({
     resolvers,
     typeDefs: schema,
+    introspection: true,
+    //@ts-ignore
+    plugins: [ApolloServerPluginLandingPageProductionDefault({ embed: true })]
 });
 
 export default startServerAndCreateNextHandler(server);
